cd489a5322d308bf0beeb4e91407ee4f07287baa
eskurinhodev <manoads2019@gmail.com>
2021-10-03 12:21:56 -0300

    criado módulo que busca o link do avatar de um usuário do github

---
+++ libpython/github_api.py
@@ -0,0 +1,15 @@
+import requests
+
+usuario = str(input('Digite o nome de usuário: '))
+def buscar_avatar(usuario):
+    """
+    Busca o avatar de um usuário do github
+    :param usuario: String contendo o nome de usuário no github
+    :return: string com o link do avatar
+    """
+
+    url = f'https://api.github.com/users/{usuario}'
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
+if __name__ == '__main__':
+    print(buscar_avatar(usuario))
