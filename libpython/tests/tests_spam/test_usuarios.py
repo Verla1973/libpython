1511d63fb269e75e9c6e70343aa90d7b6cdb53d6
eskurinhodev <manoads2019@gmail.com>
2021-10-04 17:09:48 -0300

    Alterado o gestor de dependências para pipenv

--- libpython/tests/tests_spam/test_usuarios.py
+++ libpython/tests/tests_spam/test_usuarios.py
@@ -1,4 +1,7 @@
+import pytest
+
 from libpython.spam.db import Conexao
+from libpython.spam.db import Sessao
 from libpython.spam.models import Usuario
 
 
@@ -6,20 +9,12 @@ def test_salvar_usuario():
     conexao = Conexao()
     sessao = conexao.gerar_sessao()
     usuario = Usuario(nome ='Eskurinho Dev')
-    sessao.salvar(usuario)
+    sessao_obj.salvar(usuario)
     assert isinstance(usuario.id, int)
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
 
 
-def test_listar_usuarios():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
+def test_listar_usuarios(sessao):
     usuarios = [Usuario(nome ='Eskurinho Dev'), Usuario(nome ='Verlã Dev')]
     for usuario in usuarios:
-        sessao.salvar(usuario)
-    assert usuarios == sessao.listar()
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
+        sessao_obj.salvar(self=usuario)
+    assert usuarios == sessao_obj.listar()
